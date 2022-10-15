714d31c1a16511bb29db2fb35b10cc72ba4124ab
Elizaveta Pernikova <lisapernikowa@mail.ru>
2022-10-15 19:37:45 +0500

    рабочий ввод массива чисел

--- lab5_4/Program.cs
+++ lab5_4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Lab5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Количество чисел в массиве: ");
+            int count = int.Parse(Console.ReadLine());
+            double[] numbers = new double [count];
+
+            Console.Write($"Введите массив из {count} элементов через пробел: ");
+            string[] str = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = double.Parse(str[i]);
+            }
+
+            foreach (double num in numbers)
+                Console.Write(num + " ");
+                
+            
+        }
+    }
+}
